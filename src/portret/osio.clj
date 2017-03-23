8d78b3e0d8ecf8f196c434108831e18af2156c3d
Johan Mynhardt <johanmynhardt@gmail.com>
2017-03-23 21:40:21 +0200

    Move OS IO/command runner to own namespace.

---
+++ src/portret/osio.clj
@@ -0,0 +1,11 @@
+(ns portret.osio)
+
+(defn execute
+  "Executes the command and arguments and returns the
+   output as a string to the caller."
+  [cmd & args]
+  ;(println (str "args: " args))
+  (let [cmdargs (into-array String (cons cmd  args))
+        ;_ (println (str "cmdargs: " cmdargs))
+        p (.exec (Runtime/getRuntime) cmdargs)]
+    (slurp (.getInputStream p))))
